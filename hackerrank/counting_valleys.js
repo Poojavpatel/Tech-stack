198aee09cc06437fcced124ee1c2ef14cd1d0570
Pooja Patel <poojavpatel71@gmail.com>
2019-06-21 12:39:45 -0700

    trying to solve counting valleys

--- hackerrank/counting_valleys.js
+++ hackerrank/counting_valleys.js
@@ -1,6 +1,7 @@
 class Stack{
     constructor(){
-        this.items=[]
+        this.items=[];
+        this.count=0;
     }
     push(item){
         this.items.push(item);
@@ -14,17 +15,31 @@ class Stack{
     {
         return this.items[this.items.length - 1]; 
     }
+    stacklength(){
+        return this.items.length;
+    }
 }
+const mountain = new Stack();
+const valley = new Stack();
 
 function countValley(n,s){
-    console.log(s)
-    if(s[0]=='U'){
-
-        while
-    }elseif(s[0] == 'D'){
-
+    var nextind = 0;
+    console.log(s);
+    if(s[nextind] == 'U'){
+        mountain.push('U');
+        var i=1;
+        while(mountain.stacklength() && i<n){
+            (s[i] == 'U') ? mountain.push('U') : mountain.pop();
+            console.log('Mountain:',mountain.items);
+            i=i+1;
+        }
+        mountain.count += 1;
+        nextind = i;
+        console.log('Next index',nextind);
     }
+    elseif()
 };
 
-countValley(8,['D','D','U','U','U','U','D','D']);
-countValley(8,['U','D','D','D','U','D','U','U']);
+// countValley(8,['D','D','U','U','U','U','D','D']);
+// countValley(8,['U','D','D','D','U','D','U','U']);
+countValley(12,['U','U','U','D','U','D','D','D','D','D','U','U']);
